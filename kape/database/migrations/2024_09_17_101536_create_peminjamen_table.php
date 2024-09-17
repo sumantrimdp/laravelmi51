edd407fc218139fd7913a83ee281b2dbbe80d176
EDOOOG <sumantri_2225110007@mhs.mdp.ac.id>
2024-09-17 18:37:25 +0700

    migreasen

---
+++ kape/database/migrations/2024_09_17_101536_create_peminjamen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->unsignedBigInteger('user_id_peminjam');
+            $table->foreign('user_id_peminjam')->references('id')->on('users');
+            $table->foreignId('komputer_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjamen');
+    }
+};
